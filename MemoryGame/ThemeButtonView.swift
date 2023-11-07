5c3015f6f0dc6012a006735fb47805f272fc8055
Smoofky <141446663+Smoofky@users.noreply.github.com>
2023-11-07 11:12:11 +0100

    Update ThemeButtonView.swift

--- MemoryGame/ThemeButtonView.swift
+++ MemoryGame/ThemeButtonView.swift
@@ -4,10 +4,11 @@ struct ThemeButtonView: View {
     @State var number: Int
     @State var image: String
     @Binding var theme: Int
-    
+    @Binding var emojiArray : [String]
     var body: some View {
         Button {
             theme = number
+            emojiArray.shuffle()
         } label: {
             VStack {
                 Image(systemName: image)
