2418229262e754b6f15efd6defbd250699a90231
Smoofky <141446663+Smoofky@users.noreply.github.com>
2023-11-07 10:34:25 +0100

    Update CardView.swift

--- MemoryGame/CardView.swift
+++ MemoryGame/CardView.swift
@@ -1,28 +1,25 @@
-
 import SwiftUI
 
 struct CardView: View {
-    @State var isFlipped: Bool
-    var icon: String
+    @State var isFlipped: Bool = false
+    let icon : String
     
     var body: some View {
-        Group {
-            ZStack {
+        ZStack {
+            Group {
                 RoundedRectangle(cornerRadius: 12)
-                    .fill(isFlipped ? .blue : .white)
-                    .strokeBorder(.blue, lineWidth: 2)
-                    
+                    .fill(.white)
+                    .strokeBorder(lineWidth: 2)
                 Text(icon)
                     .font(.largeTitle)
-                    .opacity(isFlipped ? 0 : 1)
             }
+            .opacity(isFlipped ? 1 : 0)
+            RoundedRectangle(cornerRadius: 12)
+                .fill()
+                .opacity(isFlipped ? 0 : 1)
         }
         .onTapGesture {
             isFlipped.toggle()
         }
     }
-}
-
-#Preview {
-    CardView(isFlipped: true, icon: "🫠")
 }
